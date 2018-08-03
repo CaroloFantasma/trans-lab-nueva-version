33f31eb8712055889ccc73503df0231cccb9f9a3
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-03 11:03:26 -0400

    creación de test listos

---
+++ test/calculate.specs.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+global.window = global;
+require('../src/js/calculate');
+
+describe('Calcular tarifa', () => { //Describe lo que habrá dentro de cada función
+    describe('calculate', () => {
+        it('debería ser una función', () => {
+            assert.equal(typeof calculate, 'function');
+            
+        });
+
+        it('Debería retornar 40 para saldo inicial 800 con tarifa alta 760', () => {
+          assert.equal(calculate(800, 760), 40);
+          assert.equal(validateEmail("39fh94j9"), false);
+      });
+  
+});
+});
--- test/main.specs.js
+++ test/main.specs.js
@@ -3,13 +3,16 @@ global.window = global;
 require('../src/js/main');
 
 describe('Validar contraseña', () => { //Describe lo que habrá dentro de cada función
-    describe('Debería verificar que la contraseña sea solo de números', () => {
-        it('La contraseña debería tener solo 8 números', () => {
-            assert.equal(validateEmail("12345678"), true);
-            assert.equal(validateEmail("abcdefgh"), false);
-            assert.equal(validateEmail("39fh94j9"), false);
+    describe('Debería verificar la cantidad de números de la contraseña', () => {
+        it('La contraseña debería tener máximo 8 números', () => {
+            assert.equal(validateEmail(12345678), true);
+            assert.equal(validateEmail(12345), true);
         });
+
+        it('La contraseña debería tener solo números', () => {
+          assert.equal(validateEmail("abcdefgh"), false);
+          assert.equal(validateEmail("39fh94j9"), false);
+      });
   
-    
 });
 });
