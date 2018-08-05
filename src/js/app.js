7ad60bc889cc42557e8709f9e73296df2b3e5de9
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-05 13:34:12 -0400

    Cambios de pantallas listos y funcionando =)

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,56 @@
-function selecScreen() {
+function selectScreen() {
   document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'block';
+  document.getElementById('menuNav').style.display = 'block';
+};
+
+function showBalance() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'none';
+  document.getElementById('screenThree').style.display = 'none';
+  document.getElementById('screenFour').style.display = 'none';
+  document.getElementById('screenFive').style.display = 'none';
+  document.getElementById('screenSix').style.display = 'block';
+  document.getElementById('menuNav').style.display = 'block';
+};
+
+function showPrice() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'none';
+  document.getElementById('screenThree').style.display = 'none';
+  document.getElementById('screenFour').style.display = 'none';
+  document.getElementById('screenSix').style.display = 'none';
+  document.getElementById('screenFive').style.display = 'block';
+  document.getElementById('menuNav').style.display = 'block';
+};
+
+function showProfile() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'none';
+  document.getElementById('screenFour').style.display = 'none';
+  document.getElementById('screenFive').style.display = 'none';
+  document.getElementById('screenSix').style.display = 'none';
+  document.getElementById('screenThree').style.display = 'block';
+  document.getElementById('menuNav').style.display = 'block';
+};
+
+function showQuestions() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'none';
+  document.getElementById('screenThree').style.display = 'none';
+  document.getElementById('screenFive').style.display = 'none';
+  document.getElementById('screenSix').style.display = 'none';
+  document.getElementById('screenFour').style.display = 'block'
+  document.getElementById('menuNav').style.display = 'block';
+};
+
+function showHome() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenThree').style.display = 'none';
+  document.getElementById('screenFour').style.display = 'none';
+  document.getElementById('screenFive').style.display = 'none';
+  document.getElementById('screenSix').style.display = 'none';
+  document.getElementById('menuNav').style.display = 'none';
   document.getElementById('screenTwo').style.display = 'block';
   document.getElementById('menuNav').style.display = 'block';
 };
