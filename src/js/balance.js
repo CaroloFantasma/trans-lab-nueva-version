870ee045f0a9b0e14389567726c4d72e7020b0c7
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-07 23:53:53 -0400

    Cálculo listo y funcionando =D

--- src/js/balance.js
+++ src/js/balance.js
@@ -1,57 +1,24 @@
-//Fetch, muestra info de tarjeta bip ingresada en el input de ver saldo
+//Fetch, muestra saldo de tarjeta bip ingresada o seleccionada
 
 btnBalance.addEventListener('click', () => {
   let bipNumber = cardNumberBip.value;
   let dataBip = dataBipContainer.value;
-fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${bipNumber || dataBip}`)
-  .then(response => response.json())
-  .then(data => {
-    infoBip = data;
-    balanceInfoBip = data.saldoTarjeta;  
-    dataContainer.innerHTML = `
+  fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${bipNumber || dataBip}`)
+    .then(response => response.json())
+    .then(data => {
+      infoBip = data;
+      balanceInfoBip = data.saldoTarjeta;
+      dataContainer.innerHTML = `
     <p class="totalBalance">Saldo total: ${data['saldoTarjeta']}</p>
     <p class="totalBalance">Fecha saldo: ${data['fechaSaldo']}</p>
     `;
-  })
-  .catch(error => {
-    console.error("No fue posible completar la solicitud.");
-  });
-  });
-
-  selectionPrice.addEventListener('click', () => {
-  let selectedPrice = document.getElementById('selectionPrice');
-  let priceValue = selectedPrice[selectedPrice.selectedIndex].value;
-  let lastPrice = price.innerHTML = '$' + priceValue;
-  
+    })
+    .catch(error => {
+      console.error("No fue posible completar la solicitud.");
+    });
 });
 
-btnCalculate.addEventListener('click', () => {
-  let bipNun = cardNumber.value;
-  let selectedChoice = selectCard.value;
 
-  fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${bipNun || selectedChoice}`)
-  .then(response => response.json())
-  .then(data => {
-    dataBipCard = data;;
-  })
-  .catch(error => {
-    console.error("No fue posible completar la solicitud.");
-  });
-});
-
-let bipCardTotal = (data) => {
-  for (let i in data) {
-    let regex = /(\d+)/g;
-    const bipBalance = data.saldoTarjeta.match(regex)
-    const finalBipBalance = parseInt(bipBalance[0] + bipBalance[1]);
-    const selectedPrice = document.getElementById('selectionPrice');
-    let valueChoice = selectedPrice[selectedPrice.selectedIndex].value;
-    const finalTotal = finalBipBalance - valueChoice;
-    console.log(finalTotal);
-    
-    total.innerHTML = '$' + finalTotal;
-  }
-}
 
 
 
--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,62 +1,30 @@
-// //FETCH que me muestra el saldo
-// function infoBip() {
-//   let number = document.getElementById('numberCards').value;
-//   let numberOfOption = document.getElementById('listCards').value;
+//Imprime la tarifa en pantalla
+selectionPrice.addEventListener('click', () => {
+  let selectedPrice = document.getElementById('selectionPrice');
+  let priceValue = selectedPrice[selectedPrice.selectedIndex].value;
+  let lastPrice = price.innerHTML = '$' + priceValue;
+});
 
-//   fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${number || numberOfOption}`)
-//     .then(response => response.json())
-//     .then(bipJSON => {
-//       numCard(bipJSON);
-//     })
-//     .catch(error => {
-//       console.error("No pudimos obtener respuesta a su solicitud");
-//       console.error("ERROR > " + error.stack);
-//     });
-//   number.innerHTML = '';
-// }
+//Fetch
+btnCalculate.addEventListener('click', () => {
+  let bipNun = cardNumber.value;
+  let selectedChoice = selectCard.value;
+  fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${bipNun || selectedChoice}`)
+    .then(response => response.json())
+    .then(bipData => {
+      dataBipCard = bipData;
+      console.log(dataBipCard);
 
-// // recorriendo api bip y mostrando en pantalla el Saldo
-// const numCard = (bipJSON) => {
-//   for (let i in bipJSON) {
-//     var regex = /(\d+)/g;
-//     const saldoBip = bipJSON.saldoTarjeta.match(regex)
-//     const saldoBipOk = parseInt(saldoBip[0] + saldoBip[1]);
-//     document.getElementById('contenedorSaldos').innerHTML = '$' + saldoBipOk;
-//   };
-// }
-
-// // Me muestra la tarifa de los distintos horarios
-// const selectOptions = () => {
-//   const selector = document.getElementById('tarifa');
-//   let valueRate = selector[selector.selectedIndex].value;
-//   document.getElementById('contenedorTarifa').innerHTML = '$' + valueRate;
-// }
-// //FETCH CALCULO DE TARIFA 
-// function infoTarifa() {
-//   let number2 = document.getElementById('numberCards2').value;
-//   let numberOfOption2 = document.getElementById('tarjeta').value;
-
-//   fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${number2 || numberOfOption2}`)
-//     .then(response => response.json())
-//     .then(bipJSON => {
-//       finalRate(bipJSON);
-//     })
-//     .catch(error => {
-//       console.error("No pudimos obtener respuesta a su solicitud");
-//       console.error("ERROR > " + error.stack);
-//     });
-//   number2.innerHTML = '';
-// }
-
-// //Me muestra el resultado final, saldo menos la tarifa.
-// const finalRate = (bipJSON) => {
-//   for (let i in bipJSON) {
-//     var regex = /(\d+)/g;
-//     const saldoBip = bipJSON.saldoTarjeta.match(regex)
-//     const saldoBipOk = parseInt(saldoBip[0] + saldoBip[1]);
-//     const selector = document.getElementById('tarifa');
-//     let valueRate = selector[selector.selectedIndex].value;
-//     const total = saldoBipOk - valueRate;
-//     document.getElementById('contenedorSaldoFinal').innerHTML = '$' + total;
-//   }
-// };
+      //Cálculo de saldo y tarifa
+      const regex = /(\d+)/g;
+      const bipBalance = dataBipCard.saldoTarjeta.match(regex)
+      const finalBipBalance = parseInt(bipBalance[0] + bipBalance[1]);
+      const selectedPrice = document.getElementById('selectionPrice').value;
+      const finalTotal = finalBipBalance - selectedPrice;
+      console.log(finalTotal);
+      totalResult.innerHTML = '$' + finalTotal;
+    })
+    .catch(error => {
+      console.error("No fue posible completar la solicitud.");
+    });
+});
